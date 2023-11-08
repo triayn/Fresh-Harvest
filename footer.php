e712c38d81c25008ca4667aba501013e92e100d4
Tria Yunita <triaynta@gmail.com>
2023-11-08 22:01:55 +0700

    sixth

---
+++ footer.php
@@ -0,0 +1,22 @@
+<div class="row align-items-center justify-content-lg-between">
+    <div class="col-lg-6 mb-lg-0 mb-4">
+        <div class="copyright text-center text-sm text-muted text-lg-start">
+            © <script>
+                document.write(new Date().getFullYear())
+            </script>,
+            made with <i class="fa fa-heart"></i> by
+            <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Tria Y</a>
+            <!-- for a better web. -->
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+            <li class="nav-item">
+                <a href="https://instagram.com/triaynta_?utm_medium=copy_link" class="nav-link text-muted" target="_blank"><i class="fab fa-instagram fa-lg"> Instagram</i></a>
+            </li>
+            <li class="nav-item">
+                <a href="https://github.com/triayn" class="nav-link text-muted" target="_blank"><i class="github-button"> Github</i></a>
+            </li>
+        </ul>
+    </div>
+</div>
